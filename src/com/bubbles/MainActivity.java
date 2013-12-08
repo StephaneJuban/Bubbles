78bcd1e42abfc9867985ea26717e9f84878000db
Stephane Juban <stephane.juban@gmail.com>
2013-12-08 11:24:57 +0100

    Add buttons and title OnClick button print console log

--- src/com/bubbles/MainActivity.java
+++ src/com/bubbles/MainActivity.java
@@ -8,13 +8,24 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
-	private Button sign_in = null; 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		final Button log_in_button = (Button) findViewById(R.id.log_in_button);
+		log_in_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	System.out.println("Log in button pressed");
+            }
+        });
+		
+		final Button sign_up_button = (Button) findViewById(R.id.sign_up_button);
+		sign_up_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				System.out.println("Sign up button pressed");
+			}
+		});
 	}
 
 	@Override
